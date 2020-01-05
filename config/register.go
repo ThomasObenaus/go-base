225823ad6f1ed55d453c54a46c608669f6cdc3b7
Thomas Obenaus <obenaus.thomas@gmail.com>
2020-01-05 19:07:15 +0100

    .Update cleanup

--- config/register.go
+++ config/register.go
@@ -7,17 +7,17 @@ import (
 	"github.com/spf13/pflag"
 )
 
-func (cfg *Provider) registerEnvParams() error {
+func (p *Provider) registerEnvParams() error {
 	replacer := strings.NewReplacer("-", "_", ".", "_")
-	cfg.Viper.SetEnvKeyReplacer(replacer)
+	p.Viper.SetEnvKeyReplacer(replacer)
 
 	// register also the config file parameter
-	if err := registerEnv(cfg.Viper, cfg.envPrefix, cfg.configFileEntry); err != nil {
+	if err := registerEnv(p.Viper, p.envPrefix, p.configFileEntry); err != nil {
 		return err
 	}
 
-	for _, entry := range cfg.configEntries {
-		if err := registerEnv(cfg.Viper, cfg.envPrefix, entry); err != nil {
+	for _, entry := range p.configEntries {
+		if err := registerEnv(p.Viper, p.envPrefix, entry); err != nil {
 			return err
 		}
 	}
@@ -25,40 +25,40 @@ func (cfg *Provider) registerEnvParams() error {
 	return nil
 }
 
-func (cfg *Provider) registerAndParseFlags(args []string) error {
+func (p *Provider) registerAndParseFlags(args []string) error {
 
 	// register also the config file parameter
-	if err := registerFlag(cfg.pFlagSet, cfg.configFileEntry); err != nil {
+	if err := registerFlag(p.pFlagSet, p.configFileEntry); err != nil {
 		return err
 	}
 
-	for _, entry := range cfg.configEntries {
-		if err := registerFlag(cfg.pFlagSet, entry); err != nil {
+	for _, entry := range p.configEntries {
+		if err := registerFlag(p.pFlagSet, entry); err != nil {
 			return err
 		}
 	}
 
-	if err := cfg.pFlagSet.Parse(args); err != nil {
+	if err := p.pFlagSet.Parse(args); err != nil {
 
 		if err == pflag.ErrHelp {
 			os.Exit(0)
 		}
 		return err
 	}
-	cfg.Viper.BindPFlags(cfg.pFlagSet)
+	p.Viper.BindPFlags(p.pFlagSet)
 
 	return nil
 }
 
-func (cfg *Provider) setDefaults() error {
+func (p *Provider) setDefaults() error {
 
 	// regard also the config file parameter
-	if err := setDefault(cfg.Viper, cfg.configFileEntry); err != nil {
+	if err := setDefault(p.Viper, p.configFileEntry); err != nil {
 		return err
 	}
 
-	for _, entry := range cfg.configEntries {
-		if err := setDefault(cfg.Viper, entry); err != nil {
+	for _, entry := range p.configEntries {
+		if err := setDefault(p.Viper, entry); err != nil {
 			return err
 		}
 	}
