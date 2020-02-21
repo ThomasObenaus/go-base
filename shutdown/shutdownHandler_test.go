c9114530b965a12f3df0476c53245a312a2a7075
Thomas Obenaus <obenaus.thomas@gmail.com>
2020-02-21 09:15:56 +0100

    [13] .Fix test

--- shutdown/shutdownHandler_test.go
+++ shutdown/shutdownHandler_test.go
@@ -42,6 +42,7 @@ func Test_ShutdownHandler(t *testing.T) {
 	stopable1.EXPECT().Stop().Return(fmt.Errorf("ERROR"))
 	stopable2.EXPECT().String().Return("stopable2")
 	stopable2.EXPECT().Stop().Return(nil)
+	h.wg.Add(1)
 	go h.shutdownHandler(shutDownChan, logger)
 
 	start := time.Now()
@@ -51,6 +52,7 @@ func Test_ShutdownHandler(t *testing.T) {
 	}()
 
 	h.WaitUntilSignal()
+	time.Sleep(time.Millisecond * 100)
 
 	// THEN
 	assert.WithinDuration(t, start, time.Now(), time.Millisecond*1200)
@@ -76,6 +78,7 @@ func Test_RegisterFront(t *testing.T) {
 		stopable1.EXPECT().String().Return("stopable1"),
 		stopable1.EXPECT().Stop().Return(fmt.Errorf("ERROR")),
 	)
+	h.wg.Add(1)
 	go h.shutdownHandler(shutDownChan, logger)
 
 	start := time.Now()
@@ -88,6 +91,7 @@ func Test_RegisterFront(t *testing.T) {
 	h.Register(stopable2)
 
 	h.WaitUntilSignal()
+	time.Sleep(time.Millisecond * 100)
 
 	// THEN
 	assert.WithinDuration(t, start, time.Now(), time.Millisecond*1200)
@@ -113,6 +117,7 @@ func Test_RegisterBack(t *testing.T) {
 		stopable2.EXPECT().String().Return("stopable2"),
 		stopable2.EXPECT().Stop().Return(nil),
 	)
+	h.wg.Add(1)
 	go h.shutdownHandler(shutDownChan, logger)
 
 	start := time.Now()
@@ -125,6 +130,7 @@ func Test_RegisterBack(t *testing.T) {
 	h.Register(stopable2, false)
 
 	h.WaitUntilSignal()
+	time.Sleep(time.Millisecond * 100)
 
 	// THEN
 	assert.WithinDuration(t, start, time.Now(), time.Millisecond*1200)
